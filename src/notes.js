b2059e6d498a09c5338f1e82ed79f829a8614f7f
Abdusyakur <abdusyakur06@gmail.com>
2024-04-28 22:54:27 +0700

    initial  commit

---
+++ src/notes.js
@@ -0,0 +1,3 @@
+const nptes = []
+
+module.exports = notes
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const {addNoteHandler, getAllNotesHandler, getNoteByIdHandler, deleteNoteByIdHandler, editNoteByIdHandler} = require('./handler')
+const routes = [{
+    method: 'POST',
+    path: '/notes',
+    hendler: addNoteHandler,
+},
+{
+    method: 'GET',
+    path: '/notes',
+    hendler: getAllNotesHandler,
+ },
+ {
+     method: 'GET',
+    path: '/notes/{id}',
+    hendler: getNoteByIdHandler ,
+ },
+ {
+    method:'PUT',
+    path:'/notes/{id}',
+    handler: editNoteByIdHandler,
+ },
+ {
+    method:'DELETE',
+    path:'/notes/{id}',
+    handler: deleteNoteByIdHandler,
+ }
+]
+
+ module.exports = routes
+
